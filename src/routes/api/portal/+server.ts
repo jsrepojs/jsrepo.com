87dfc2887c1a812d728d68cb5bf3110c8154de07
Aidan Bleser <aidanbleser35@gmail.com>
2025-04-21 07:27:23 -0500

    customer portal

---
+++ src/routes/api/portal/+server.ts
@@ -0,0 +1,23 @@
+import { POLAR_ACCESS_TOKEN } from '$env/static/private';
+import { auth } from '$lib/auth';
+import { redirectToLogin } from '$lib/auth/redirect';
+import { getUser } from '$lib/backend/db/functions';
+import { CustomerPortal } from '@polar-sh/sveltekit';
+import assert from 'assert';
+
+export const GET = CustomerPortal({
+	accessToken: POLAR_ACCESS_TOKEN,
+	getCustomerId: async (event) => {
+		const session = await auth.api.getSession({ headers: event.request.headers });
+
+		if (!session) redirectToLogin(event.url);
+
+		const user = await getUser(session.user.id);
+
+		assert(user !== null, 'User should be defined!');
+		assert(user.polarCustomerId !== null, 'User should have a customer id!');
+
+		return user.polarCustomerId;
+	},
+	server: 'sandbox'
+});
