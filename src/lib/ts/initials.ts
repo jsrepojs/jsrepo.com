15d9f7120490e427feae25715d864e0df7d28b56
Aidan Bleser <aidanbleser35@gmail.com>
2025-06-15 12:44:56 -0500

    weird edge case

--- src/lib/ts/initials.ts
+++ src/lib/ts/initials.ts
@@ -1,4 +1,6 @@
-export function getInitials(name: string) {
+export function getInitials(name: string | undefined) {
+	if (!name) return '?';
+
 	const [first, last] = name.split(' ');
 
 	return `${first?.[0]}${last?.[0] ?? ''}`.toUpperCase();
